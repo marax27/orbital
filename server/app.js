5c031d122190404de33da3cffc20fee1da51a902
marax27 <kacpert314@gmail.com>
2022-01-12 22:03:14 +0100

    Enable compression

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const compression = require('compression');
 
 const port = 8100;
 
 const app = express();
+app.use(compression({ filter: shouldCompress }))
+
 app.use(express.static('../orbital/dist'));
 app.listen(port, () => console.log(`Listening on port ${port}.`));
+
+function shouldCompress(req, res) {
+    if (req.headers['x-no-compression']) {
+        return false;
+    }
+
+    return compression.filter(req, res);
+}
